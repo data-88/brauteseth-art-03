09c1653b0b2f35d16b2cb90103bbdc8932bf4dc9
data-88 <andrew@4cast.co.za>
2020-07-21 15:29:43 +0200

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,44 +1,32 @@
-import React from "react"
-import { graphql, StaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import "../style/normalize.css"
-import "../style/all.scss"
+import "../style/normalize.css";
+import "../style/all.scss";
 
 const AboutPage = ({ data }, location) => {
-  const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
     <Layout title={siteTitle}>
-      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <SEO
+        title="About"
+        keywords={[`Johannesburg`, `blog`, `guy with camera`, `Photographer`]}
+      />
 
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
             Keen on photos. Loves writing. Here for you.
           </h2>
-          <figure className="kg-card kg-image-card kg-width-full">
-            <Img
-              fluid={data.benchAccounting.childImageSharp.fluid}
-              className="kg-image"
-            />
-            <figcaption>About Andrew Brauteseth and Guy With Camera</figcaption>
-          </figure>
           <h3 id="dynamic-styles">Dynamic Photographer</h3>
           <p>
-            I've been taking photos all my life but began doing gigs professionally about 10 years ago.{" "}
-            <a href="https://work.guywithcamera.co.za/">
-              My work website.
-            </a>{" "}
-          </p>
-          <p>
-            Don't forget to check out my {" "}
-            <a href="https://www.instagram.com/guy_with_camera/">
-              Instagram.
-            </a>{" "}
+            Don't forget to check out my{" "}
+            <a href="https://www.instagram.com/guy_with_camera/">Instagram.</a>{" "}
             for more ways to connect.
           </p>
           <p>
@@ -50,8 +38,8 @@ const AboutPage = ({ data }, location) => {
         </div>
       </article>
     </Layout>
-  )
-}
+  );
+};
 
 const indexQuery = graphql`
   query {
@@ -70,7 +58,7 @@ const indexQuery = graphql`
       }
     }
   }
-`
+`;
 
 export default props => (
   <StaticQuery
@@ -79,4 +67,4 @@ export default props => (
       <AboutPage location={props.location} data={data} {...props} />
     )}
   />
-)
+);
