ea2c91d80148af8a65b06507ccb25470d0fe5ed2
data-88 <andrew@4cast.co.za>
2020-07-21 14:29:02 +0200

    git problems fixed

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Guy With Camera",
   shortName: "GWC",
   description: "This side of the camera.",
-  url: "https://www.guywithcamera.co.za", // Domain of your site without prefix!
+  url: "https://guywithcamera.co.za", // Domain of your site without prefix!
   prefix: "/",
   author: "Andrew Brauteseth",
   twitter: "guy_with_camera"
