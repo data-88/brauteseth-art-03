109854738aa4ad6c14b53af42b54384d417e2c91
data-88 <andrew@4cast.co.za>
2020-07-21 14:27:39 +0200

    this side

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'Guy With Camera',
-  shortName: 'GWC',
-  description: 'The other side of the camera.',
-  url: 'https://www.guywithcamera.co.za', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Andrew Brauteseth',
-  twitter: 'guy_with_camera'
-}
+  name: "Guy With Camera",
+  shortName: "GWC",
+  description: "This side of the camera.",
+  url: "https://www.guywithcamera.co.za", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Andrew Brauteseth",
+  twitter: "guy_with_camera"
+};
