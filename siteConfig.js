48521f12d83d47c7b4f1fae6fd12853a7bbcc369
data-88 <andrew@thecyberium.com>
2021-01-08 15:08:14 +0200

    head

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: "Andrew Brauteseth",
-  shortName: "Brauteseth",
-  description: "Art in all it's forms.",
+  name: "Artist",
+  shortName: "Andrew Brauteseth",
+  description: "Mediums and their messages.",
   url: "https://brauteseth.art", // Domain of your site without prefix!
   prefix: "/",
   author: "Andrew Brauteseth",
